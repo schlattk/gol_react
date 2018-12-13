9f45263051a1de49b5065bd5e948d070c9d14aaa
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2018-12-13 17:08:16 +0000

    changed component tests with describe

--- src/tests/Square.test.js
+++ src/tests/Square.test.js
@@ -3,19 +3,21 @@ import Enzyme from 'enzyme';
 import {shallow} from 'enzyme';
 import index from '../index';
 import Adapter from 'enzyme-adapter-react-16';
-
 Enzyme.configure({ adapter: new Adapter() });
 
-test('Square changes state after click', () => {
-  const square = shallow(<index.Square item={'O'} />);
-  expect(square.state().alive).toEqual(false);
-  square.simulate('Click');
-  expect(square.state().alive).toEqual(true);
-});
-
-test('Square changes class after click', () => {
-    const square = shallow(<index.Square item={'X'} />);
-    expect(square.props().className).toEqual('square--green');
+describe('ControlForm', () => {
+  let square;  
+  beforeEach( () => {
+    square = shallow(<index.Square item={'O'} />); 
+  })
+  test('Square changes state after click', () => {
+    expect(square.state().alive).toEqual(false);
     square.simulate('Click');
+    expect(square.state().alive).toEqual(true);
+   });
+  test('Square changes class after click', () => {
     expect(square.props().className).toEqual('square--grey');
+    square.simulate('Click');
+    expect(square.props().className).toEqual('square--green');
+   });
 });
--- src/tests/form.test.js
+++ src/tests/form.test.js
@@ -7,21 +7,34 @@ import Adapter from 'enzyme-adapter-react-16';
 import ControlForm from '../form'
 Enzyme.configure({ adapter: new Adapter() });
 
-test('form state changes after click', () => {  
-  spyOn(index.printer, 'print');  
-  const form = mount(<ControlForm />);
-  expect(form.state().action).toEqual('');
-  let button = form.find('.make');
-  button.simulate('click');
-  expect(form.state().action).toEqual('make');
-  button = form.find('.random');
-  button.simulate('click');
-  expect(form.state().action).toEqual('random');  
-  button = form.find('.stop');
-  button.simulate('click');
-  expect(form.state().action).toEqual('stop');   
-  button = form.find('.run');
-  button.simulate('click');
-  expect(form.state().action).toEqual('run');  
+describe('ControlForm', () => {
+  let form; 
+  let button;  
+  beforeEach( () => {
+    spyOn(index.printer, 'print');  
+    form = mount(<ControlForm />);
+    })
+  test('initial form state', () => {  
+    expect(form.state().action).toEqual('');
+  });
+  test('form state changes after click - make', () => { 
+    button = form.find('.make');
+    button.simulate('click');
+    expect(form.state().action).toEqual('make'); 
+  });
+  test('form state changes after click - random', () => { 
+    button = form.find('.random');
+    button.simulate('click');
+    expect(form.state().action).toEqual('random'); 
+  });
+  test('form state changes after click - stop', () => { 
+    button = form.find('.stop');
+    button.simulate('click');
+    expect(form.state().action).toEqual('stop'); 
+  });
+  test('form state changes after click - run', () => { 
+    button = form.find('.run');
+    button.simulate('click');
+    expect(form.state().action).toEqual('run'); 
+  });
 });
-
