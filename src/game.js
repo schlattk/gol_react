32bfe1ba684330cc5458c61d14162a757ceffd17
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2019-01-03 10:54:03 +0000

    refactored conditional in game.js

--- src/game.js
+++ src/game.js
@@ -11,23 +11,25 @@ class Game {
     this.make = false;
   };
   seed (number) {
-    this.grid.forEach((item) => { for(let j = 0; j < item.length; j++){
+    this.grid.forEach(item => {for(let j = 0; j < item.length; j++){
                                 item[j] = randomizer.random(number); }
                               }
                     );
   };
   move () {
     if (this.make) { this.grid = index.extractor.extract() }
-    let length = this.grid.length;
-    let counter_grid = new Array(length).fill(new Array(length));
-    for(let i = 0; i < length; i ++){ counter_grid[i] = new Array(length) };
-    counter_grid.forEach((item, i) => { for(let j = 0; j < length; j++){item[j] = scanner.scan(this.grid,i,j); }});
-    this.grid.forEach((item, i) => { for(let j = 0; j < length; j++){
-                                    if(this.grid[i][j] === 'X'){counter_grid[i][j] < 2 || counter_grid[i][j] > 3 ? this.grid[i][j] = 'O' : this.grid[i][j] = 'X' }
-                                    else if (this.grid[i][j] === 'O') {counter_grid[i][j] === 3 ? this.grid[i][j] = 'X' : this.grid[i][j] = 'O'}
-                                    }}
-                      );
-  };
+    let g = this.grid;
+    let len = g.length;
+    let scan_grid = new Array(len).fill(new Array(len));
+    for(let i = 0; i < len; i ++){ scan_grid[i] = new Array(len) };
+    scan_grid.forEach((item, i) => { for(let j = 0; j < len; j++){item[j] = scanner.scan(g,i,j) }});
+    g.forEach((item, i) => { item.forEach((val, j) => {
+                            let field = scan_grid[i][j];
+                            if(field === 3){ g[i][j] = 'X' }
+                            else if(field < 2 || field > 3){ g[i][j] = 'O'}
+                          })
+                        });
+    };
   print () {
     index.printer.print(helper.flatten(this.grid));
   };
