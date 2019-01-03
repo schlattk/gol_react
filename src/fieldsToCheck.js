4e421f27dd04172e164de16b01e5cf3cc71a712b
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2019-01-03 14:16:19 +0000

    replace for loop with map in game and refactored scanner.js

--- src/fieldsToCheck.js
+++ src/fieldsToCheck.js
@@ -5,8 +5,10 @@ const fieldsToCheck = (function () {
     let rep = helper.rep;
     let len = grid.length - 1;
     let fields = [grid[x][rep(y-1, len)], grid[x][rep(y+1, len)]];
-    grid[x-1] ? fields.push(grid[x-1][y], grid[x-1][rep(y-1, len)], grid[x-1][rep(y+1, len)]) : fields.push(grid[x+len][y], grid[x+len][rep(y-1, len)], grid[x+len][rep(y+1, len)]);
-    grid[x+1] ? fields.push(grid[x+1][y], grid[x+1][rep(y+1, len)], grid[x+1][rep(y-1, len)]) : fields.push(grid[x-len][y], grid[x-len][rep(y+1, len)], grid[x-len][rep(y-1, len)]);
+    if(grid[x-1]) { fields.push(grid[x-1][y], grid[x-1][rep(y-1, len)], grid[x-1][rep(y+1, len)]) }
+    else { fields.push(grid[x+len][y], grid[x+len][rep(y-1, len)], grid[x+len][rep(y+1, len)]) };
+    if(grid[x+1]) {fields.push(grid[x+1][y], grid[x+1][rep(y+1, len)], grid[x+1][rep(y-1, len)]) }
+    else { fields.push(grid[x-len][y], grid[x-len][rep(y+1, len)], grid[x-len][rep(y-1, len)]) };
     return fields
   };
   return { fields :checkFields }
--- src/game.js
+++ src/game.js
@@ -3,23 +3,21 @@ import helper from './helper.js';
 import randomizer from './randomizer.js';
 import scanner from './scanner.js';
 class Game {
-  constructor(height, width){
+  constructor (height, width){
     this.height = height;
     this.width = width;
-    this.grid = new Array(height);
-    for(let i = 0; i < height; i++){ this.grid[i] = new Array(width) };
+    this.grid = new Array(height).fill('').map(x => x = new Array(width).fill(''));
     this.make = false;
   };
   seed (number) {
-    let randGrid = helper.flatten(this.grid).fill('').map(x => x = randomizer.random(number));
+    let randGrid = helper.flatten(this.grid).map(x => x = randomizer.random(number));
     this.grid = helper.chunk(randGrid, Math.pow(randGrid.length, 0.5));
   };
   move () {
     if (this.make) { this.grid = index.extractor.extract() }
     let g = this.grid;
     let len = g.length;
-    let scanG = new Array(len).fill(new Array(len));
-    for(let i = 0; i < len; i ++){ scanG[i] = new Array(len) };
+    let scanG = new Array(len).fill('').map(x => x = new Array(len).fill(''));
     scanG.forEach((item, i) => { for(let j = 0; j < len; j++){ item[j] = scanner.scan(g, i, j) }});
     g.forEach((item, i) => { for(let j = 0; j < len; j++){
                             let field = scanG[i][j];
